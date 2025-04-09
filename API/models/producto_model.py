f0d1b173b47d6e23e977b16e747445c3723d20a0
Migueljara20 <trabajosm20@gmail.com>
2025-04-09 03:20:28 -0500

    Delete API directory

--- API/models/producto_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-#valida los datos con pydantic
-class ProductoBase(BaseModel):
-    id: int
-    nombre: str
-    descripcion: str
-    precio: float
-    stock: int
